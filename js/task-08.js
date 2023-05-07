243f1a645200bd4363d6cd557efb23ebbe3a0ba0
DmytroErmakov <demchik22@gmail.com>
2023-05-07 13:04:06 +0300

    run task08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  // const formData = new FormData(event.currentTarget);
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value.trim() || !password.value.trim()) {
+    return alert("Внимание! необходимо заполнить все поля :)");
+  } else {
+    const formData = { email: email.value, password: password.value };
+    console.log(formData);
+  }
+  
+  form.reset();
+}
 
